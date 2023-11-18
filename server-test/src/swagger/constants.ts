fb09eef26084d1c8d8436ffd7be95741f6628319
duymanh3602 <82275607+duymanh3602@users.noreply.github.com>
2023-11-18 14:57:35 +0700

    feat: setup swagger

---
+++ server-test/src/swagger/constants.ts
@@ -0,0 +1,4 @@
+export const SWAGGER_API_ROOT = 'api/docs';
+export const SWAGGER_API_NAME = 'KLTN CỦA ĐINH MẠNH ^^';
+export const SWAGGER_API_DESCRIPTION = 'API Description For My-Thesis Project';
+export const SWAGGER_API_CURRENT_VERSION = '1.0';
